7f83053f4de4109238f24ce3370abed8519d8b45
J.J. Idt & Moof Mayeda <student@epicodus.com>
2014-07-25 10:24:51 -0700

    calculate the cost to ship a parcel

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -3,8 +3,12 @@ class Parcel
     @dim1 = dim1
     @dim2 = dim2
     @dim3 = dim3
+    @weight = weight
   end
   def volume
     @dim1*@dim2*@dim3
+  end
+  def shipping_cost
+    self.volume*@weight/100
   end
 end
