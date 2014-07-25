857c57594efe385f345cad649617a74cb07e41f1
J.J. Idt & Moof Mayeda <student@epicodus.com>
2014-07-25 11:06:56 -0700

    add functionality to calculate total cost of multiple shipments

--- shipping.rb
+++ shipping.rb
@@ -1,4 +1,5 @@
 require './lib/parcels'
+@costs = []
 
 def calculate_shipping
   puts "what is the length of your package?"
@@ -10,7 +11,32 @@ def calculate_shipping
   puts "what is the weight of your package?"
   weight = gets.chomp.to_f
   your_parcel = Parcel.new(length, width, height, weight)
-  puts "It will cost #{your_parcel.shipping_cost.round(2)} dollars"
+  @costs << your_parcel.shipping_cost.round(2)
+  puts "\nIt will cost $#{your_parcel.shipping_cost.round(2)}"
+  main_menu
 end
 
-calculate_shipping
+def total_cost
+  @costs.inject(:+)
+end
+
+def main_menu
+  puts "Would you like to ship a package?"
+  puts "\nTo ship a package, enter 's'. \n\nFor the total cost of all your packages, enter 't'. \n\nTo exit, type 'x'.\n"
+  response = gets.chomp
+  if response == "s"
+    calculate_shipping
+  elsif response == "t"
+    puts "Your total cost is $ #{total_cost}\n"
+    main_menu
+  elsif response == "x"
+    puts "no problem, come back when you have a package you'd like to ship!"
+    exit
+  else
+    main_menu
+  end
+end
+
+puts "Welcome to JJ & Moof's postal service!"
+
+main_menu
